5d7b945ab28a0a6aa464f03a0f090ddfbe29eb69
Alexia Sander <sander.alexia@gmail.com>
2023-06-27 22:45:18 +0900

    some style added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,12 +5,24 @@ class QuestionsController < ApplicationController
 
   def answer
     # coach logic in answer action
-    @answer = if params[:query] == 'I am going to work'
-                'Great!'
-              elsif params[:query].end_with?('?')
-                'Silly question, get dressed and go to work!'
-              else
-                "I don't care, get dressed and go to work!"
-              end
+    @question = params[:query]
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
+
+  # def answer
+  #   # coach logic in answer action
+  #   @answer = if params[:query] == 'I am going to work'
+  #               'Great!'
+  #             elsif params[:query].end_with?('?')
+  #               'Silly question, get dressed and go to work!'
+  #             else
+  #               "I don't care, get dressed and go to work!"
+  #             end
+  # end
